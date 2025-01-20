514739bda5c90b018df8a8e614d9fc32b1b1d120
ANDR0ID-DCS <125316494+ANDR0ID-DCS@users.noreply.github.com>
2025-01-20 14:09:54 -0800

    Add files via upload
    Updates OH-58D
    Adds CH-47F

--- wwt/ufcPatch/aircraft/ufcPatchCH47F.lua
+++ wwt/ufcPatch/aircraft/ufcPatchCH47F.lua
@@ -1,61 +1,51 @@
 local ufcUtils = require("ufcPatch\\utilities\\ufcPatchUtils")
 
-ufcPatchOH58D = {}
+ufcPatchCH47F = {}
 
-function ufcPatchOH58D.generateUFCData()
+function ufcPatchCH47F.generateUFCData()
+
 
 local MainPanel = GetDevice(0)
-local UHFRadio = GetDevice(30)
-local FMRadio1 = GetDevice(29)
-local VHFRadio = GetDevice(31)
-local FMRadio2 = GetDevice(32)
-
-local PwrSwpos = MainPanel:get_argument_value(248) --OH58 Battery 1 switch 
-local MasterArm = MainPanel:get_argument_value(171) 
---local ICPRotarySwitch = MainPanel:get_argument_value(188) --Now usin utility function 
-local Switch13 = MainPanel:get_argument_value(14)
-local Switch24 = MainPanel:get_argument_value(16)
-local Switch5 = MainPanel:get_argument_value(17)
-
-local ArmStatus = "S" 
+local PwrSwpos = MainPanel:get_argument_value(559) --CH47 Battery switch 
+--local ICPRotarySwitch = MainPanel:get_argument_value(613) --613 is Pilot ICP --Now using utility function
+local UHFRadio = GetDevice(47) --ARC-164 UHF
+local FMRadio1 = GetDevice(49) --ARC-201 FM1
+local VHFRadio = GetDevice(48) --ARC-186 VHF
+--local FMRadio2 = GetDevice(32)--ARC-201 FM2 --*Update When Second Radio Implimented* 
+local HFRadio = GetDevice(50) --ARC-220 HF
 
 	if PwrSwpos == 1 then
 
-	FC3RadarAltitudeString = (ufcPatchUtils.GenaricRadarAltitudeFeet()-2) --Account for 2ft airframe offset 
+	FC3RadarAltitudeString = (ufcPatchUtils.GenaricRadarAltitudeFeet()-7) --Account for 7ft airframe offset 
 	HeadingString = ufcPatchUtils.MagHeading()
 	AirspeedString = ufcPatchUtils.AirspeedKnots()
-	FuelString = ((ufcPatchUtils.TotalFuel()/100)*736) -- Conversion to lbs
+	FuelString = ((ufcPatchUtils.TotalFuel()/100)*6734) -- Conversion to lbs
 	Vertical = ufcPatchUtils.VerticalV_FPMv2()
 	FlareCount = ufcPatchUtils.flares()
-	ICPRotarySwitch = ufcPatchUtils.SwtichPosition(188, 10)
+	ChaffCount = ufcPatchUtils.chaff()
+	ICPRotarySwitch = ufcPatchUtils.SwtichPosition(613, 20) 
 	
 	FM1Freq = FMRadio1:get_frequency()
 	UHFFreq = UHFRadio:get_frequency()
 	VHFFreq = VHFRadio:get_frequency()
-	FM2Freq = FMRadio2:get_frequency()
+	--FM2Freq = FMRadio2:get_frequency() --*Update When Second Radio Implimented* 
+	HFFreq = HFRadio:get_frequency()
 	
-		--Armament Status 
-		if (MasterArm == 1) then 
-			ArmStatus = "A" 
-		elseif (MasterArm == 0) then 
-			ArmStatus = "B"
-		elseif (MasterArm == -1) then
-			ArmStatus = "S" 
-		end 	
-		
 	elseif PwrSwpos ~= 1 then 
 	FC3RadarAltitudeString = ""
 	HeadingString = ""
 	AirspeedString = ""
 	FuelString = ""
 	Vertical = ""
-	ArmStatus = ""
-	RadioDisplay1 = ""
+	FlareCount = ""
+	ChaffCount = ""
+	RadioDisplay = ""
 	RadioDisplay1 = ""
 	RadioDisplay2 = ""	
 	end 
-	
---Radios
+
+--Radios 
+
 	--AN/ARC 201 FM1
 	local FM1digits = { math.floor(FM1Freq / 1000) }
 
@@ -91,79 +81,76 @@ local ArmStatus = "S"
 		end
 		VHFString = VHFString .. VHFdigitToAppend
 	end
-
-	--AN/ARC 201 FM2
-	local FM2digits = { math.floor(FM2Freq / 1000) }
-
-	local FM2String = ""
-	for index, value in ipairs(FM2digits) do
-		local FM2digitToAppend = value
-		if value >= 88000 then
-			FM2digitToAppend = 88000
-		end
-		FM2String = FM2String .. FM2digitToAppend
-	end
 	
-	--Remote Radio
-	if Switch13 == 1 then --FM1 Remote
-		RemoteRadioFreq = FM1String
-	elseif Switch13 == -1 then --VHF Remote 
-		RemoteRadioFreq = VHFString
-	elseif Switch24 == 1 then --UHF Remote
-		RemoteRadioFreq = UHFString
-	elseif Switch24 == -1 then --Not Implimented 
-		RemoteRadioFreq = "000000"
-	elseif Switch5 == -1 then --FM2 Remote
-		RemoteRadioFreq = FM2String
-	elseif Switch13 == 0 and Switch24 == 0 and Switch5 == 0 then 
-		RemoteRadioFreq = ""
+	--AN/ARC 220 HF
+	local HFdigits = { math.floor(HFFreq / 1000) }
+
+	local HFString = ""
+	for index, value in ipairs(HFdigits) do
+		local HFdigitToAppend = value
+		if value >= 30000 then
+			HFdigitToAppend = 30000
+		end
+		HFString = HFString .. HFdigitToAppend
 	end
-		
+ 
 	--Pilot ICP
 	local ICPdigits = {(ICPRotarySwitch * 1)}
 
 	local ICPdisplay = ""
 	for index, value in ipairs(ICPdigits) do
 		local ICPdisplayToAppend = value
-		if value >= 7 then
-			ICPdisplayToAppend = 7
+		if value >= 10 then
+			ICPdisplayToAppend = 10
 		end
 		ICPdisplay = ICPdisplay .. ICPdisplayToAppend
 		if value == 0 then
 			RadioDisplay = "000000"
-			RadioDisplay1 = "P"
-			RadioDisplay2 = "V"
-		elseif value == 1 then 
-			RadioDisplay = "000000"
 			RadioDisplay1 = "I"
 			RadioDisplay2 = "C"
-		elseif value == 2 then
+		elseif value == 1 then
 			RadioDisplay = FM1String
 			RadioDisplay1 = "F"
 			RadioDisplay2 = "1"
-		elseif value == 3 then
+		elseif value == 2 then
 			RadioDisplay = UHFString
 			RadioDisplay1 = "U"
 			RadioDisplay2 = "H"
-		elseif value == 4 then
-			RadioDisplay = VHFString 
+		elseif value == 3 then
+			RadioDisplay = VHFString
 			RadioDisplay1 = "V"
 			RadioDisplay2 = "H"
+		elseif value == 4 then
+			RadioDisplay = HFString
+			RadioDisplay1 = "H"
+			RadioDisplay2 = "F"
 		elseif value == 5 then
-			RadioDisplay = "000000"
-			RadioDisplay1 = "S"
-			RadioDisplay2 = "A"
-		elseif value == 6 then
-			RadioDisplay = FM2String
+			RadioDisplay = "05"--FM2String
 			RadioDisplay1 = "F"
 			RadioDisplay2 = "2"
-		elseif value == 7 then
-			RadioDisplay = RemoteRadioFreq 
+		elseif value == 6 then
+			RadioDisplay = "06"
+			RadioDisplay1 = ""
+			RadioDisplay2 = ""
+		elseif value == 7 then --Needs Update?
+			RadioDisplay = "07"
+			RadioDisplay1 = ""
+			RadioDisplay2 = ""
+		elseif value == 8 then
+			RadioDisplay = "08"--RemoteRadioFreq 
 			RadioDisplay1 = "R"
 			RadioDisplay2 = "T"
+		elseif value == 9 then --Needs Update?
+			RadioDisplay = "09"
+			RadioDisplay1 = "B"
+			RadioDisplay2 = "U"
+		elseif value == 10 then
+			RadioDisplay = "000000"
+			RadioDisplay1 = "P"
+			RadioDisplay2 = "V"		
 		end
 	end
-	
+ 
 	--Send info to UFC components 
     return ufcUtils.buildSimAppProUFCPayload({
         scratchPadNumbers=RadioDisplay, --Radio
@@ -173,11 +160,11 @@ local ArmStatus = "S"
 		option4=AirspeedString,   
         option5=Vertical, --VSI
 		com1=FlareCount, --Flare
-		com2=ArmStatus, --Arm
+		com2=ChaffCount, --Chaff
 		scratchPadString1=RadioDisplay1, --Radio Type
 		scratchPadString2=RadioDisplay2  --Radio Type 
     })
 end
 
-return ufcPatchOH58D --v1.0 by ANDR0ID 
+return ufcPatchCH47F --v1.0 by ANDR0ID 
 
